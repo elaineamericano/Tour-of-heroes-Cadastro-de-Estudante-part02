a4b2a9fad4e1a1144d99f34bffdb98ed0ad938a7
elaineamericano <61953473+elaineamericano@users.noreply.github.com>
2021-12-13 06:03:54 -0300

    Cadastro estudante prt2

---
+++ src/app/mock-estudantes.ts
@@ -0,0 +1,14 @@
+import { Estudante } from './estudante';
+
+export const ESTUDANTES: Estudante[] = [
+  { id: 11, name: 'Alessandro Reis' },
+  { id: 12, name: 'Elaine Alves' },
+  { id: 13, name: 'Yasmim Ruth' },
+  { id: 14, name: 'Isabelly Caroline' },
+  { id: 15, name: 'Hellena Reis' },
+  { id: 16, name: 'Gercina da Silva' },
+  { id: 17, name: 'Alceu Rodrigues' },
+  { id: 18, name: 'Tamires Gouveia' },
+  { id: 19, name: 'Raimunda Alice' },
+  { id: 20, name: 'Daiane Americano' }
+];
